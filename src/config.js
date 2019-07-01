55ed807966f469daa4132c6d8e986683b0972d4c
Rob (Personal) <robpersonal@robertoembp15tb.lan>
2019-07-01 06:16:13 -0700

    Prep first deploy

--- src/config.js
+++ src/config.js
@@ -1,7 +1,8 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DB_URL: process.env.DB_URL || 'postgresql://backlogger@localhost/backlogger',
+  DB_URL:
+    process.env.DATABASE_URL || 'postgresql://backlogger@localhost/backlogger',
   TEST_DB_URL: 'postgresql://postgres@localhost/backlogger-test',
   CLIENT_ORIGIN_LOCAL: 'http://localhost:3000/',
   CLIENT_ORIGIN_PROD: 'https://backlogger-app.rosborne132.now.sh/',
